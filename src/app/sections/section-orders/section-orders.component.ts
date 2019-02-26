081bba358ec53728f39ad03deab81f0c32e25ee3
Shiva Sharma <shivathebravo@gmail.com>
2019-02-25 20:11:50 -0500

    added orders template

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Order } from '../../shared/orders';
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
@@ -8,6 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {
+      id: 1,
+      customer: {
+        id: 1,
+        name: "Shiva Sharma",
+        state: "MD", email: "shiva@shiva.com"
+      },
+      total: 240,
+      placed: new Date(2019, 2, 2),
+      fulfilled: new Date(2019, 2, 4),
+      status: 'Completed'
+    },
+    {
+      id: 2,
+      customer: {
+        id: 1,
+        name: "John Doe",
+        state: "MD",
+        email: "jdoe@shiva.com"
+      },
+      total: 500,
+      placed: new Date(2019, 2, 2),
+      fulfilled: new Date(2019, 2, 4),
+      status: 'Completed'
+
+    }
+
+  ]
 
   ngOnInit() {
   }
